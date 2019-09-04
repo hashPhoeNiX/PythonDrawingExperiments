90e31151bc397646adada0e87ce85eb6640ca9b8
Francis Joseph Serina <francis.serina@gmail.com>
2019-09-04 10:34:38 +0800

    Automated creation of spirograph with different ratios

--- spirograph.py
+++ spirograph.py
@@ -3,31 +3,37 @@ import math
 from PIL import Image, ImageDraw
 
 imageDimensions = (2048,2048)
-canvas = Image.new('RGBA', imageDimensions, (0, 0, 0, 255))
-
-painter = ImageDraw.Draw(canvas)
-
 numSubdivisions = 1080
 
 center = (imageDimensions[0] / 2, imageDimensions[1] / 2)
 radius = [512, 256]
-ratios = [1.0, 2.0]
 
-tInc = 1 / numSubdivisions
-points = []
-for i in range(numSubdivisions + 1):
-    t = i * tInc
-    angleRad = t * 2 * math.pi * ratios[0]
-    point = ( center[0] + radius[0] * math.cos(angleRad),
-        center[1] + radius[0] * math.sin(angleRad) )
-    angleRad = (-t * 2 * math.pi) * (ratios[1] - 1)
-    point = ( point[0] + radius[1] * math.cos(angleRad),
-        point[1] + radius[1] * math.sin(angleRad) )
-    points.append(point)
+start = 1
+end = 21
 
-colors = util.color_gradient(numSubdivisions)
-for i in range(numSubdivisions):
-    painter.line([points[i], points[i+1]], fill=colors[i], width=2)
+for j in range(start, end):
+    for k in range(j, end):
+        canvas = Image.new('RGBA', imageDimensions, (0, 0, 0, 255))
+        painter = ImageDraw.Draw(canvas)
 
-#canvas.save(util.get_filename(__file__))
-canvas.show()
+        ratios = [j, k]
+        tInc = 1 / numSubdivisions
+        points = []
+        for i in range(numSubdivisions + 1):
+            t = i * tInc
+            angleRad = t * 2 * math.pi * ratios[0]
+            point = ( center[0] + radius[0] * math.cos(angleRad),
+                center[1] + radius[0] * math.sin(angleRad) )
+            angleRad = (-t * 2 * math.pi) * (ratios[1] - 1)
+            point = ( point[0] + radius[1] * math.cos(angleRad),
+                point[1] + radius[1] * math.sin(angleRad) )
+            points.append(point)
+
+        colors = util.color_gradient(numSubdivisions)
+        for i in range(numSubdivisions):
+            painter.line([points[i], points[i+1]], fill=colors[i], width=2)
+
+        canvas.save( "out/spirograph_%(ratio1)02d-%(ratio2)02d.png" %
+            {"ratio1":ratios[0], "ratio2":ratios[1]} )
+        #canvas.save(util.get_filename(__file__))
+        #canvas.show()
