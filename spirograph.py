8c1e4e6f73f04ecd89f16cf880bc84c09592b801
Francis Joseph Serina <francis.serina@gmail.com>
2019-09-05 21:41:28 +0800

    Cleanup

--- spirograph.py
+++ spirograph.py
@@ -6,28 +6,27 @@ imageDimensions = (2048,2048)
 numSubdivisions = 1080
 
 center = (imageDimensions[0] / 2, imageDimensions[1] / 2)
-radius = [512, 256]
+radius = [512, 512]
 
 start = 1
 end = 21
 
 for j in range(start, end):
-    for k in range(j, end):
-        if util.gcd(j, k - 1) != 1:
+    for k in range(j+1, end):
+        if util.gcd(j, k) != 1:
             continue
 
         canvas = Image.new('RGBA', imageDimensions, (0, 0, 0, 255))
         painter = ImageDraw.Draw(canvas)
 
-        ratios = [j, k]
         tInc = 1 / numSubdivisions
         points = []
         for i in range(numSubdivisions + 1):
             t = i * tInc
-            angleRad = t * 2 * math.pi * ratios[0]
+            angleRad = t * 2 * math.pi * j
             point = ( center[0] + radius[0] * math.cos(angleRad),
                 center[1] + radius[0] * math.sin(angleRad) )
-            angleRad = (-t * 2 * math.pi) * (ratios[1] - 1)
+            angleRad = (-t * 2 * math.pi) * k
             point = ( point[0] + radius[1] * math.cos(angleRad),
                 point[1] + radius[1] * math.sin(angleRad) )
             points.append(point)
@@ -37,4 +36,4 @@ for j in range(start, end):
             painter.line([points[i], points[i+1]], fill=colors[i], width=2)
 
         canvas.save( "out/spirograph_%(ratio1)02d-%(ratio2)02d.png" %
-            {"ratio1":ratios[0], "ratio2":ratios[1]} )
+            {"ratio1":j, "ratio2":k} )
