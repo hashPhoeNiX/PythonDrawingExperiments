2697f5a635188996601a17ba166f93476dc5e0a6
Francis Joseph Serina <francis.serina@gmail.com>
2019-09-06 19:03:02 +0800

    Updated harmonograph
    Changed dimensions of canvas to 1080p
    Added a scale to t
    Corrected range of frequencies
    Added comment to output per frame

--- harmonograph.py
+++ harmonograph.py
@@ -2,7 +2,7 @@ import util
 import math
 from PIL import Image, ImageDraw
 
-imageDimensions = (2048,2048)
+imageDimensions = (1920,1080)
 canvas = Image.new('RGBA', imageDimensions, (0, 0, 0, 255))
 
 painter = ImageDraw.Draw(canvas)
@@ -10,25 +10,37 @@ painter = ImageDraw.Draw(canvas)
 center = (imageDimensions[0] / 2, imageDimensions[1] / 2)
 
 points = []
-amp = [768, 768]
-ampDamp = [.00006, .00005]
-freq = [3, 5]
-phaseDeg = [0, 30]
-approxZero = 0.5
+amp = [480, 480, 270, 270]
+dampFactor = [.000006, .00001, 0.00001, 0.00001]
+freq = [.099, .298, .0995, .3]
+phaseDeg = [0, 0, 45, 60]
+minThreshold = 0.5
 
 t = 0
-while math.exp(-ampDamp[0] * t) > approxZero and math.exp(-ampDamp[1] * t) > approxZero:
+while True:
     x = center[0]
     y = center[1]
-    x += amp[0] * math.sin(util.deg_to_rad(freq[0] * t + phaseDeg[0])) * math.exp(-ampDamp[0] * t)
-    y += amp[1] * math.sin(util.deg_to_rad(freq[1] * t + phaseDeg[1])) * math.exp(-ampDamp[1] * t)
+    numTerms = int(len(amp) / 2)
+    terminate = False
+    for i in range(numTerms):
+        damp = math.exp(-dampFactor[i] * t)
+        x += amp[i] * math.sin(util.deg_to_rad(freq[i] * t + phaseDeg[i])) * damp
+        if (damp < minThreshold):
+            terminate = True
+        damp = math.exp(-dampFactor[numTerms + i] * t)
+        y += amp[numTerms + i] * math.sin(util.deg_to_rad(freq[numTerms + i] * t + phaseDeg[numTerms + i])) * damp
+        if (damp < minThreshold):
+            terminate = True
     points.append((x, y))
-    t += 1
+    t += 8
+    if terminate:
+        break
 
-print(t)
-colors = util.color_gradient(t)
-for i in range(t - 1):
+print("Number of points: %d" % len(points))
+colors = util.color_gradient(len(points) - 1)
+for i in range(len(points) - 1):
     painter.line([points[i], points[i+1]], fill=colors[i], width=2)
+    #canvas.save("out/harmonograph_%(frame)06d.png" % {"frame":i})
 
 #canvas.save(util.get_filename(__file__))
 canvas.show()
