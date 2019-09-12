2e77a6def4ccd06a31fd2099396e4e81cb077a74
Francis Joseph Serina <francis.serina@gmail.com>
2019-09-12 22:28:00 +0800

    Generalized curve for epi/hypotochroid

--- epihypocycloid.py
+++ epihypocycloid.py
@@ -9,10 +9,11 @@ painter = ImageDraw.Draw(canvas)
 
 center = (imageDimensions[0] / 2, imageDimensions[1] / 2)
 
-radius = [150, 140]
-rho = int( math.fabs(radius[1]) / math.gcd( radius[0], int( math.fabs(radius[1]) ) ) )
+radius = [400, -110]
+h = radius[1] - 30
+rho = int( math.fabs(radius[1]) / math.gcd( int( math.fabs(radius[0]) ), int( math.fabs(radius[1]) ) ) )
 maxAngleRad = 2 * math.pi * rho
-numSubdivisions = 120 * rho
+numSubdivisions = 360 * rho
 angleRadInc = maxAngleRad / numSubdivisions
 points = []
 colors = util.color_gradient(numSubdivisions)
@@ -23,14 +24,14 @@ for t in range(numSubdivisions + 1):
     y = center[1]
     x += (radius[0] + radius[1]) * math.cos(angleRad)
     y += (radius[0] + radius[1]) * math.sin(angleRad)
-    x += radius[1] * math.cos((radius[0] + radius[1]) / radius[1] * angleRad)
-    y += radius[1] * math.sin((radius[0] + radius[1]) / radius[1] * angleRad)
+    x += h * math.cos((radius[0] + radius[1]) / radius[1] * angleRad)
+    y += h * math.sin((radius[0] + radius[1]) / radius[1] * angleRad)
     points.append((x, y))
 
 print("Number of points: %d" % len(points))
 for i in range(len(points) - 1):
     painter.line([points[i], points[i+1]], fill=colors[i], width=2)
-    canvas.save("out/%(frame)06d.png" % {"frame":i})
+    #canvas.save("out/%(frame)06d.png" % {"frame":i})
 
 #canvas.save("out/" + util.get_filename(__file__))
-#canvas.show()
+canvas.show()
