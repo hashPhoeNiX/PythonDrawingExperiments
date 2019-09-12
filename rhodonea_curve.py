54b0476584a0ea8f9679819d970306ee6511b94a
Francis Joseph Serina <francis.serina@gmail.com>
2019-09-12 22:28:37 +0800

    Clean up rose curve

--- rhodonea_curve.py
+++ rhodonea_curve.py
@@ -2,32 +2,34 @@ import util
 import math
 from PIL import Image, ImageDraw
 
-imageDimensions = (2048,2048)
+imageDimensions = (1920,1080)
 canvas = Image.new('RGBA', imageDimensions, (0, 0, 0, 255))
 
 painter = ImageDraw.Draw(canvas)
 
 center = (imageDimensions[0] / 2, imageDimensions[1] / 2)
 
-radius = 1024
-kNum = 7
-kDen = 13
+radius = 270
+kNum = 3
+kDen = 1
 kDenReduced = int(kDen / util.gcd(kNum, kDen))
 maxAngleRad = math.pi * kDenReduced
-numSubdivisions = 180 * kDenReduced
+numSubdivisions = 360 * kDenReduced
 angleRadInc = maxAngleRad / numSubdivisions
 points = []
 colors = util.color_gradient(numSubdivisions)
 
-for t in range(numSubdivisions):
+for t in range(numSubdivisions + 1):
     angleRad = t * angleRadInc
     r = radius * math.cos(kNum / kDen * angleRad)
     x = center[0] + r * math.cos(angleRad)
     y = center[1] + r * math.sin(angleRad)
     points.append((x, y))
 
-for i in range(numSubdivisions - 1):
+print("Number of points: %d" % len(points))
+for i in range(len(points) - 1):
     painter.line([points[i], points[i+1]], fill=colors[i], width=2)
+    #canvas.save("out/%(frame)06d.png" % {"frame":i})
 
 #canvas.save("out/" + util.get_filename(__file__))
 canvas.show()
